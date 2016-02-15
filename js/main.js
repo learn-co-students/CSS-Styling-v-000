b0bee17bc2a2db05b2814530bb49fc21ff63886a
jbok4 <jbok4@aol.com>
2016-02-14 19:47:49 -0500

    using the code from the intro to web dev class with jon grover, added in full completed website

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+
+/////// mobile menu ////////////
+
+$('a.menu-icon').click(function() {
+	$('#menu-links').slideToggle();
+});
+
+$(window).resize(function() {
+	if ($(window).width() > 700) {
+		$('menu-links').removeAttr('style');
+	}
+});
+
+});
